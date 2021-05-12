269b1a761b37f6b2482e04077ca48d81e3f6b4ab
Caitlyn <caitlynmiley.cm@gmail.com>
2021-05-12 08:13:44 -0500

    added data to the table

--- src/components/Table.js
+++ src/components/Table.js
@@ -5,7 +5,32 @@ import React from 'react';
 function Table({ data }) {
     return (
         <div>
-            {JSON.stringify(data)}
+            <table style={{width: "100%"}}>
+                <tr>
+                    <th>
+                        Name
+                    </th>
+                    <th>
+                        Gender
+                    </th>
+                    <th>
+                        Email
+                    </th>
+                </tr>
+                {data && data.results.map(user => 
+                    <tr>
+                        <td>
+                            {user.name.first + " " + user.name.last}
+                        </td>
+                        <td>
+                            {user.gender}
+                        </td>
+                        <td>
+                            {user.email}
+                        </td>
+                    </tr>
+                )}
+            </table>
         </div>
     )
 }
